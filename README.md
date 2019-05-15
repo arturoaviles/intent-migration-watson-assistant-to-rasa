eb1c02cbf89413565df1469723ec890dcd7f2bdf
Arturo Avilés <artcastell@gmail.com>
2019-05-15 12:07:09 -0500

    Add Intent Migration tool to transform Watson Assistant Workspace Intents JSON format to Rasa Markdown format

---
+++ README.md
@@ -0,0 +1,18 @@
+# Intent Migration from Watson Assistant/Conversation to Rasa
+
+This repo allows you to migrate the *IBM Watson Assistant/Conversation Workspace* __Intents__ from the __JSON format__ to the *Rasa* __Markdown format__.
+
+## Steps
+
+1. Clone this repo.
+2. Download the Watson Assistant Workspace JSON file.
+3. Run __python migrate-intents.py <WATSON_ASSISTANT_WORKSPACE>.json__
+4. Check the __nlu.md__ file in the directory.
+
+## Credits
+
+@arturoaviles
+
+## License
+
+MIT License
---
+++ migrate-intents.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+
+def main():
+    workspace_name = sys.argv[2]
+
+    markdown_file = open("nlu.md", "w+")
+
+    with open(workspace_name, 'r') as workspace_json:
+        workspace = json.load(workspace_json)
+        intents = workspace["intents"]
+
+        for intent in intents:
+            intent_name = intent["intent"]
+            intent_examples = intent["examples"]
+            print("\n## intent:", intent_name)
+            intent_md_str = "\n## intent: " + intent_name + "\n"
+            markdown_file.write(intent_md_str)
+            for intent_example in intent_examples:
+                intent_example_text = intent_example["text"]
+                print(" -", intent_example_text)
+                example_md_str = " - " + intent_example_text + "\n"
+                markdown_file.write(example_md_str)
+
+        markdown_file.close()
+
+
+if __name__ == "__main__":
+    main()
